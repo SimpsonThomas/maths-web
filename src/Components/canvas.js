f310bdf2f6349ef94db18cd4a509c29b22e1a871
Thomas Simpson <46006748+SimpsonThomas@users.noreply.github.com>
2021-12-29 13:14:33 +0000

    Textbox added

--- src/Components/canvas.js
+++ src/Components/canvas.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef } from "react";
+import './canvas.css'
 
 const Canvas = props => {
 
@@ -28,22 +29,27 @@ const Canvas = props => {
         ctx.stroke()
     }
 
-    const grid = (ctx, colour2='white') => {
+    const grid = (ctx, colourMinor=gridProps.minorAxColour, colourMajor=gridProps.majorAxColour) => {
         let gridSize = gridProps.size
         let width = ctx.canvas.width
         let height = ctx.canvas.height    
 
-        for (let i=0; i*gridSize<=Math.max(height/2,width/2); i++) {
-            let colour = i==0 ? gridProps.majorAxColour : gridProps.minorAxColour
+        for (let i=-10; i*gridSize<=2*Math.max(height/2,width/2); i++) {
+            let colour = i==0 ? colourMajor : colourMinor
             //let colour = colour2
             // x gridlines
             drawLine(ctx, {x:-width/2,y:i*gridSize}, {x:width/2,y:i*gridSize}, colour)
-            drawLine(ctx, {x:-width/2,y:-i*gridSize}, {x:width/2,y:-i*gridSize}, colour)
+            drawLine(ctx, {x:-width,y:-i*gridSize}, {x:width,y:-i*gridSize}, colour)
 
             // y gridlines
-            drawLine(ctx, {y:-height/2,x:i*gridSize}, {y:height/2,x:i*gridSize}, colour)
-            drawLine(ctx, {y:-height/2,x:-i*gridSize}, {y:height/2,x:-i*gridSize}, colour)
+            drawLine(ctx, {y:-2*height,x:i*gridSize}, {y:2*height,x:i*gridSize}, colour)
+            drawLine(ctx, {y:-2*height,x:-i*gridSize}, {y:2*height,x:-i*gridSize}, colour)
         }
+    }
+
+    const detShape = (ctx, colour='red') => {
+        ctx.fillStyle = colour
+        ctx.fillRect(0,0,gridProps.size+1, -gridProps.size-1)
     }
 
     const shiftGrid = (ctx) => {
@@ -51,8 +57,10 @@ const Canvas = props => {
         let width = ctx.canvas.width
         let height = ctx.canvas.height 
 
-        ctx.setTransform(0.1,2,1,0.5,width/2,height/2)
-        grid(ctx)
+        //ctx.setTransform(0.1,2,1,0.5,width/2,height/2)
+        ctx.setTransform(-1,-5,-5,-3,width/2,height/2)
+        grid(ctx,'red', 'blue')
+        detShape(ctx, 'yellow')
         ctx.setTransform(1,0,0,1,width/2,height/2)
     }
 
@@ -78,6 +86,7 @@ const Canvas = props => {
             context.fillStyle = gridProps.backgroundColour
             context.fillRect(-canvas.width/2, -canvas.height/2, canvas.width, canvas.height)
             grid(context)
+            detShape(context, 'blue')
             //draw(context,frameCount)
             shiftGrid(context)
 
@@ -93,7 +102,18 @@ const Canvas = props => {
 
     }, [])
 
-    return <canvas ref={canvasRef} {...props}/>
+    const html = <>
+        <div class='matrixBox'>
+            <p class='matrixTitle'>Input Matrix Here</p>
+            <input class='matrixInput1' placeholder='1'/>
+            <input class='matrixInput2' placeholder='2'/>
+            <input class='matrixInput3' placeholder='3'/>
+            <input class='matrixInput4' placeholder='4'/>
+        </div>
+        <canvas ref={canvasRef} {...props}/>
+    </>
+
+    return html
 }
 
 export default Canvas
